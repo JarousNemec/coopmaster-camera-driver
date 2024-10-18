9892f20f914d1c660a63366b992883a9b17c2cca
JarousNemec <jarous.nemec@gmail.com>
2024-10-18 23:27:10 +0200

    Dockerfile update

--- app/configuration.py
+++ app/configuration.py
@@ -4,10 +4,10 @@ host = "127.0.0.1"
 port = 9001
 hello_message = "Hello from camera driver"
 
-log_file_name = "logger.log"
+log_file_name = "camera_driver.log"
 
 def get_log_directory():
     return "./logs/"
 
 def get_log_filename():
-    return log_file_name;
+    return log_file_name
